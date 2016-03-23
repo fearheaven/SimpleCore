1c6dfd326649ebb75288cb36f3a7f7d88ffa02ed
Sinhika <sinhika@republicofnewhome.org>
2016-03-23 12:29:59 -0500

    fixed build.gradle to insert mod version+ build.number into APIInfo.

--- src/main/java/alexndr/api/core/APIInfo.java
+++ src/main/java/alexndr/api/core/APIInfo.java
@@ -11,7 +11,7 @@ public class APIInfo {
 	public static final String ID = "simplecore";
 	public static final String NAME = "SimpleCore API";
 	public static final String DESCRIPTION = "An API for easy creation of plugins such as SimpleOres, Fusion and others.";
-	public static final String VERSION = "1.8.9-1.2.2";
+	public static final String VERSION = "@MODVERSION@";
 	public static final String URL = "http://bit.ly/SimpleOres";
 	public static final List<String> AUTHORS = Lists.newArrayList("AleXndrTheGr8st");
 	public static final String CREDITS = "Created by AleXndrTheGr8st.";
