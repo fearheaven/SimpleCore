12608a1ded51ce06d87a1adb7ba5c4e336eeb788
Sinhika <sinhika@republicofnewhome.org>
2016-03-27 18:56:11 -0500

    fixed crash when loaded on dedicated server

--- src/main/java/alexndr/api/helpers/events/ClientEventHelper.java
+++ src/main/java/alexndr/api/helpers/events/ClientEventHelper.java
@@ -12,23 +12,26 @@ import alexndr.api.helpers.game.TooltipHelper;
 /**
  * @author AleXndrTheGr8st
  */
-public class ClientEventHelper {
+public class ClientEventHelper 
+{
 	@SubscribeEvent
-	public void fovEvent(FOVUpdateEvent event) {
+	public void fovEvent(FOVUpdateEvent event) 
+	{
 		EntityPlayer player = Minecraft.getMinecraft().thePlayer;
 		float baseFOV = event.fov;
-		if(player.isUsingItem() && player.getItemInUse().getItem() instanceof SimpleBow) {
-			SimpleBow bow = (SimpleBow)player.getItemInUse().getItem();
-			int useRemaining = new ItemStack(bow).getMaxItemUseDuration() - player.getItemInUseCount();
+		if (player.isUsingItem()
+				&& player.getItemInUse().getItem() instanceof SimpleBow) {
+			SimpleBow bow = (SimpleBow) player.getItemInUse().getItem();
+			int useRemaining = new ItemStack(bow).getMaxItemUseDuration()
+					- player.getItemInUseCount();
 			float fov = baseFOV - (useRemaining * bow.getZoomAmount() / 20.0F);
-			if(fov < baseFOV - bow.getZoomAmount())
+			if (fov < baseFOV - bow.getZoomAmount())
 				fov = (baseFOV - bow.getZoomAmount());
 			event.newfov = fov;
-		}
-		else {
+		} else {
 			event.newfov = event.fov;
 		}
-	}
+	} // end fovEvent
 	
 	@SubscribeEvent
 	public void toolTipEvent(ItemTooltipEvent event) {
