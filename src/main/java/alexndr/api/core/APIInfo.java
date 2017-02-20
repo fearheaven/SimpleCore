a2c0e3deafe91840fb8ccc0820dd3df484050530
Sinhika <sinhika@republicofnewhome.org>
2017-02-20 11:42:09 -0600

    hopefully fixed Forge dependency issue

--- src/main/java/alexndr/api/core/APIInfo.java
+++ src/main/java/alexndr/api/core/APIInfo.java
@@ -19,7 +19,7 @@ public class APIInfo {
 	public static final String LOGO = "assets/simplecore/logos/simplecore.png";
 	public static final String PARENT = "";
 	public static final boolean USEDEPENDENCYINFO = true;
-	public static final String DEPENDENCIES = "required-after:Forge@[13.20.0.2228,)";
+	public static final String DEPENDENCIES = "required-after:forge@[13.20.0.2228,)";
 
 	// use mcmod.info instead, so that tools that look at the jar file such as MultiMC
 	// and possibly Curse Client can get modinfo -- Sinhika
