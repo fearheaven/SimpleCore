fb719abe7a1d5fa29a6eccb8546f9dfb088c6f04
Sinhika <sinhika@republicofnewhome.org>
2017-05-03 15:54:12 -0500

    Rewrote ConfigEntry base object, and ConfigArmor to use new system

--- src/main/java/alexndr/api/config/types/ConfigArmor.java
+++ src/main/java/alexndr/api/config/types/ConfigArmor.java
@@ -1,85 +1,61 @@
 package alexndr.api.config.types;
 
-import java.util.List;
-
-import com.google.common.collect.Lists;
+import alexndr.api.config.ConfigHelper;
+import net.minecraftforge.common.config.Configuration;
 
 /**
  * @author AleXndrTheGr8st
  */
-public class ConfigArmor extends ConfigEntry{
-	// private List<ConfigValue> valuesList = Lists.newArrayList();
-	
+public class ConfigArmor extends ConfigEntry
+{
 	//Primary armor attributes.
-	private ConfigValue durability = new ConfigValue("Durability");
-	private ConfigValue enchantability = new ConfigValue("Enchantability");
-	private ConfigValue helmReduction = new ConfigValue("HelmetReduction");
-	private ConfigValue chestReduction = new ConfigValue("ChestplateReduction");
-	private ConfigValue legsReduction = new ConfigValue("LeggingsReduction");
-	private ConfigValue bootsReduction = new ConfigValue("BootsReduction");
+	protected int durability;
+	protected int enchantability;
+	protected int helmReduction;
+	protected int chestReduction;
+	protected int legsReduction;
+	protected int bootsReduction;
 	
 	//Additional armor attributes.
-	private ConfigValue repairMaterial = new ConfigValue("RepairMaterial");
+	protected String repairMaterial;
 	
 	/**
 	 * Creates a ConfigArmor. This is for armor material sets, eg. Mythril Armor.
 	 * @param name Name of the ConfigArmor
-	 * @param category The category to place the ConfigArmor in
 	 */
-	public ConfigArmor(String name, String category) {
-		super(name, category);
-		this.valuesList.addAll(Lists.newArrayList(durability, enchantability, helmReduction, chestReduction, legsReduction, bootsReduction));
-		super.setValuesList(valuesList);
+	public ConfigArmor(String name) 
+	{
+		super(name, ConfigHelper.CATEGORY_ARMOR, true);
 	}
 	
-	@Override
-	public List<ConfigValue> getValuesList(){
-		return valuesList;
-	}
+	/**
+	 * get current settings from config file.
+	 */
+	public void GetConfig(Configuration config) 
+	{
+		repairMaterial = config.getString("RepairMaterial", subcategory, repairMaterial, 
+										  "Either ore-dictionary name or actual item name");
+		durability = config.getInt("Durability", subcategory, durability, 1, 32000, 
+								   "How many hits it can take");
+		enchantability = config.getInt("Enchantability", subcategory, enchantability, 1, 255,
+									    "higher = more enchantable");
+		helmReduction =  config.getInt("HelmReduction", subcategory, helmReduction, 1, 255,
+			    "Helm armor value");
+		chestReduction =  config.getInt("ChestReduction", subcategory, chestReduction, 1, 255,
+			    "Chest armor value");
+		legsReduction =  config.getInt("LegsReduction", subcategory, legsReduction, 1, 255,
+			    "Leggings armor value");
+		bootsReduction =  config.getInt("BootsReduction", subcategory, bootsReduction, 1, 255,
+			    "Boots armor value");	
+	} // end GetConfig()
 	
-	@Override
-	public void setValuesList(List<ConfigValue> valuesList) {
-		this.valuesList = valuesList;
-	}
-	
-	@Override
-	public ConfigValue createNewValue(String valueName) {
-		ConfigValue value = new ConfigValue(valueName);
-		value.setActive();
-		valuesList.add(value);
-		return value;
-	}
-	
-	@Override
-	public ConfigArmor createNewValue(String valueName, String dataType, String currentValue, String defaultValue) {
-		ConfigValue value = new ConfigValue(valueName);
-		value.setActive();
-		value.setDataType(dataType);
-		value.setCurrentValue(currentValue);
-		value.setDefaultValue(defaultValue);
-		valuesList.add(value);
-		return this;
-	}
-	
-	@Override
-	public ConfigValue getValueByName(String valueName) {
-		for(ConfigValue value : this.valuesList) {
-			if(value.getName().equals(valueName))
-				return value;
-		}
-		return null;
-	}
-
 	/**
 	 * Returns the durability of the armor.
 	 * @return Armor durability
 	 */
-	public int getDurability() {
-		for(ConfigValue value : valuesList) {
-			if(value.getName().equals(durability.getName()))
-				return Integer.parseInt(value.getCurrentValue());
-		}
-		return 0;
+	public int getDurability() 
+	{
+		return durability;
 	}
 
 	/**
@@ -87,8 +63,9 @@ public class ConfigArmor extends ConfigEntry{
 	 * @param durability Armor durability
 	 * @return ConfigArmor
 	 */
-	public ConfigArmor setDurability(int durability) {
-		this.durability.setActive().setDataType("@I").setCurrentValue("" + durability).setDefaultValue("" + durability);
+	public ConfigArmor setDurability(int durability) 
+	{
+		this.durability = durability;
 		return this;
 	}
 
@@ -96,12 +73,9 @@ public class ConfigArmor extends ConfigEntry{
 	 * Returns the enchantability of the armor.
 	 * @return Armor enchantability
 	 */
-	public int getEnchantability() {
-		for(ConfigValue value : valuesList) {
-			if(value.getName().equals(enchantability.getName()))
-				return Integer.parseInt(value.getCurrentValue());
-		}
-		return 0;
+	public int getEnchantability() 
+	{
+		return enchantability;
 	}
 
 	/**
@@ -109,8 +83,9 @@ public class ConfigArmor extends ConfigEntry{
 	 * @param enchantability Armor enchantability
 	 * @return ConfigArmor
 	 */
-	public ConfigArmor setEnchantability(int enchantability) {
-		this.enchantability.setActive().setDataType("@I").setCurrentValue("" + enchantability).setDefaultValue("" + enchantability);
+	public ConfigArmor setEnchantability(int enchantability) 
+	{
+		this.enchantability = enchantability;
 		return this;
 	}
 
@@ -118,12 +93,9 @@ public class ConfigArmor extends ConfigEntry{
 	 * Returns the helmet reduction of the armor.
 	 * @return Helmet reduction
 	 */
-	public int getHelmReduction() {
-		for(ConfigValue value : valuesList) {
-			if(value.getName().equals(helmReduction.getName()))
-				return Integer.parseInt(value.getCurrentValue());
-		}
-		return 0;
+	public int getHelmReduction() 
+	{
+		return helmReduction;
 	}
 
 	/**
@@ -132,7 +104,7 @@ public class ConfigArmor extends ConfigEntry{
 	 * @return ConfigArmor
 	 */
 	public ConfigArmor setHelmReduction(int helmReduction) {
-		this.helmReduction.setActive().setDataType("@I").setCurrentValue("" + helmReduction).setDefaultValue("" + helmReduction);
+		this.helmReduction = helmReduction;
 		return this;
 	}
 
@@ -141,11 +113,7 @@ public class ConfigArmor extends ConfigEntry{
 	 * @return Chest-plate reduction
 	 */
 	public int getChestReduction() {
-		for(ConfigValue value : valuesList) {
-			if(value.getName().equals(chestReduction.getName()))
-				return Integer.parseInt(value.getCurrentValue());
-		}
-		return 0;
+		return chestReduction;
 	}
 
 	/**
@@ -154,7 +122,7 @@ public class ConfigArmor extends ConfigEntry{
 	 * @return ConfigArmor
 	 */
 	public ConfigArmor setChestReduction(int chestReduction) {
-		this.chestReduction.setActive().setDataType("@I").setCurrentValue("" + chestReduction).setDefaultValue("" + chestReduction);
+		this.chestReduction = chestReduction;
 		return this;
 	}
 
@@ -162,12 +130,9 @@ public class ConfigArmor extends ConfigEntry{
 	 * Returns the leggings reduction of the armor.
 	 * @return Leggings reduction
 	 */
-	public int getLegsReduction() {
-		for(ConfigValue value : valuesList) {
-			if(value.getName().equals(legsReduction.getName()))
-				return Integer.parseInt(value.getCurrentValue());
-		}
-		return 0;
+	public int getLegsReduction() 
+	{
+		return legsReduction;
 	}
 
 	/**
@@ -176,7 +141,7 @@ public class ConfigArmor extends ConfigEntry{
 	 * @return ConfigArmor
 	 */
 	public ConfigArmor setLegsReduction(int legsReduction) {
-		this.legsReduction.setActive().setDataType("@I").setCurrentValue("" + legsReduction).setDefaultValue("" + legsReduction);
+		this.legsReduction = legsReduction;
 		return this;
 	}
 
@@ -185,11 +150,7 @@ public class ConfigArmor extends ConfigEntry{
 	 * @return Boots reduction
 	 */
 	public int getBootsReduction() {
-		for(ConfigValue value : valuesList) {
-			if(value.getName().equals(bootsReduction.getName()))
-				return Integer.parseInt(value.getCurrentValue());
-		}
-		return 0;
+		return bootsReduction;
 	}
 
 	/**
@@ -198,7 +159,7 @@ public class ConfigArmor extends ConfigEntry{
 	 * @return ConfigArmor
 	 */
 	public ConfigArmor setBootsReduction(int bootsReduction) {
-		this.bootsReduction.setActive().setDataType("@I").setCurrentValue("" + bootsReduction).setDefaultValue("" + bootsReduction);
+		this.bootsReduction = bootsReduction;
 		return this;
 	}
 	
@@ -208,11 +169,7 @@ public class ConfigArmor extends ConfigEntry{
 	 * @return Armor repair material
 	 */
 	public String getRepairMaterial() {
-		for(ConfigValue value : valuesList) {
-			if(value.getName().equals(repairMaterial.getName()))
-				return value.getCurrentValue();
-		}
-		return "";
+		return repairMaterial;
 	}
 	
 	/**
@@ -222,7 +179,7 @@ public class ConfigArmor extends ConfigEntry{
 	 * @return ConfigArmor
 	 */
 	public ConfigArmor setRepairMaterial(String repairMaterial) {
-		this.repairMaterial.setActive().setDataType("@S").setCurrentValue(repairMaterial).setDefaultValue(repairMaterial);
+		this.repairMaterial = repairMaterial;
 		return this;
 	}
-}
+} // end class
